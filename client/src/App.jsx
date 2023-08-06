af15260d6651ba5079102576fad2edcaa0130472
Jayasurya.K <jayasurya7aa@gmail.com>
2023-08-06 17:51:19 +0530

    Init the project

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { useState, useEffect } from 'react';
+import './App.css';
+
+//functions
+import { getTest } from './functions/test';
+function App() {
+  const [data, setData] = useState("Hello world");
+
+  useEffect(() => {
+    getTest()
+      .then((res) => {
+        setData(res.message);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="App">
+      <h1>{data}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,38 @@
+//import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.stripe_secret_key);
+
+const stripeSecretKey = process.env.stripe_secret_key;
+const stripePublishableKey = process.env.stripe_publishable_key;
+
+
+//app
+const app = express();
+
+//db
+mongoose
+    .connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log("DB CONNECTION ERROR",err));
+
+
+//middleware
+app.use(morgan('dev'));
+app.use(cors({origin: true,credentials: true}));
+
+//routes
+const testRoutes = require('./routes/test');
+app.use('/',testRoutes);
+//port
+const port = process.env.PORT || 8080;
+//listener
+const server = app.listen(port,()=>
+    console.log(`Server is running on port ${port}`)
+);
